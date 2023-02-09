95786935ad83b80e77436899640a24a1da94609a
jylsat11 <jylsat11@gmail.com>
2023-02-09 12:00:03 +0900

    1. 할일 목록 리스팅

---
+++ src/components/TodoMain.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="page">
+    <header><h1>Vue Fire todo1</h1></header>
+    <main>
+      <div class="todos">
+        <div class="write">
+          <input type="text"/>
+          <button class="btn add">Add</button>
+        </div>
+        <ul class="list">
+          <li v-for="todo in todos" :key="todo.text">
+            <i class="far fa-check-square"></i>
+            <span>
+              {{ todo.text }}
+              <b>
+                <a href="">Edit</a>
+                <a href="">Del</a>
+              </b>
+            </span>
+          </li>
+        </ul>
+      </div>
+    </main>
+  </div>
+ 
+</template>
+
+<script>
+export default {
+    props: ['todos'],
+    data() {
+        todos:[
+            {text: '공부하기', state: 'yet'},
+            {text: '운동하기', state: 'done'},
+            {text: '글쓰기', state: 'done'},
+        ]
+    }
+}
+</script>
+
+<style>
+
+</style>
